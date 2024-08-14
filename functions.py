5c77b26a1f588b628a56b2934afe95bfb92d77b4
Max Koller <max.koller@uib.no>
2024-08-14 19:40:16 +0200

    update

--- functions.py
+++ functions.py
@@ -1,3 +1,15 @@
+from collections import defaultdict
+import itertools
+from itertools import product
+
+
 
 def is_in_interval(number, minimum, maximum):
     return minimum <= number <= maximum
+
+def generate_coin_sample_space(num_flips=10):
+    weighted_sample_space = defaultdict(int)
+    for coin_flips in product(['Heads', 'Tails'], repeat=num_flips):
+        heads_count = len([outcome for outcome in coin_flips if outcome == 'Heads'])
+        weighted_sample_space[heads_count] += 1
+    return weighted_sample_space
